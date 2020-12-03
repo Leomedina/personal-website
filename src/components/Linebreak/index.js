4caac850e671b5b395b838b5aec65d512daec814
Leo <medinayleo@gmail.com>
2020-12-03 18:19:33 -0500

    header completed

---
+++ src/components/Linebreak/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+function LineBreak() {
+  return (
+    <div className="linebreak">
+    </div>
+  );
+};
+
+export default LineBreak;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Nav from '../../components/Nav';
 import Header from '../../components/Header';
 import Project from '../../components/Project';
+import LineBreak from '../../components/Linebreak';
 
 function Home() {
   return (
     <main>
       <Nav />
       <Header />
+      <LineBreak />
       <section className="projects">
         <Project />
       </section>
