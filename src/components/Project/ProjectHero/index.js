98e415ce70f9c366d1baebdc92f30deeecdd05c3
Leo <medinayleo@gmail.com>
2020-12-04 15:44:32 -0500

    completed basic layout

--- src/components/Project/ProjectHero/index.js
+++ src/components/Project/ProjectHero/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import stock from '../../../assets/images/stock-1.png';
+import downArrow from '../../../assets/images/down-arrow.png';
 import './style.css';
 
 function ProjectHero() {
   return (
     <div className="p-hero">
-      <div>sdafbsdyukhfb</div>
-      <div>sdafbsdyukhfb</div>
+      <img src={stock} alt="Stock Image of a Desk" />
+      <div className="p-meta">
+        <ul>
+          <li>Type: Full-Stack Project.</li>
+          <li>Technologies: Python, Flask, PostgreSLQ.</li>
+        </ul>
+        <div className="p-next">
+          <p>NEXT PROJECT</p>
+          <img src={downArrow} alt="Next Project Button" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Project/ProjectInfo/index.js
+++ src/components/Project/ProjectInfo/index.js
@@ -7,7 +7,7 @@ function ProjectInfo() {
     <div className="p-info">
       <h2>Considered Collectively</h2>
       <p>A web app that allows you to track and follow the bills in Congress that matter to you. Built using Python, Flask, Bootstrap 5, and deployed on Heroku using a PostgreSQL database.</p>
-      <span className="p-view-link">View Project</span>
+      <span className="p-view-link">VIEW PROJECT</span>
     </div>
   );
 };
